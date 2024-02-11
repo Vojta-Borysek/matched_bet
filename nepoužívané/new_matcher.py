eac52061d21d57dc25a16fb7a351810d35a63536
Vojta-Borysek <138588488+Vojta-Borysek@users.noreply.github.com>
2024-02-11 15:26:46 +0100

    Delete nepoužívané directory

--- nepoužívané/new_matcher.py
+++
@@ -1,7 +0,0 @@
-from difflib import SequenceMatcher
-
-
-def similarity(a, b):
-    return SequenceMatcher(None, a, b).ratio()
-
-print(similarity('inter - slavia praha', 'internazionale - slavia prague'))
